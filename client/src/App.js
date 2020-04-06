5d2a31179c539ed92ecf121ff010451ece719768
HoanThuat <hoanthuatenji@gmail.com>
2020-04-06 09:35:35 +0700

    first commit library management

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Index from './containers/login/index'
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  );
+}
+
+export default App;
